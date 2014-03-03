109c04c2d6e1fe596d22967b670a2b54fcc0bcb0
Harshal Bhakta <harshal.c.bhakta@gmail.com>
2014-03-03 17:48:28 +0530

    Add nginx cookbook, solo.rb & web.json

---
+++ cookbooks/nginx/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: nginx
+# Recipe:: default
+#
+# Copyright 2014, InformationWorks
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "nginx"
---
+++ solo.rb
@@ -0,0 +1,3 @@
+# http://docs.opscode.com/config_rb_solo.html
+file_cache_path "/var/chef/cache"
+cookbook_path [ './cookbooks' ]
